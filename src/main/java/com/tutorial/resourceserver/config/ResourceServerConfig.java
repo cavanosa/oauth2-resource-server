5adaefb9c9febe01764b7ecfb2b8e7d66ab8c0c4
cavanosa <cavanosa@cavanosa.cav>
2023-04-02 21:38:48 +0200

    cors

--- src/main/java/com/tutorial/resourceserver/config/ResourceServerConfig.java
+++ src/main/java/com/tutorial/resourceserver/config/ResourceServerConfig.java
@@ -3,6 +3,7 @@ package com.tutorial.resourceserver.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.jwt.JwtDecoders;
@@ -19,6 +20,7 @@ public class ResourceServerConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http)throws Exception{
+        http.cors(Customizer.withDefaults());
         return http
                 .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
                 .oauth2ResourceServer(oauth2 -> oauth2
